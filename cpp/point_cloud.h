e3126fb4e466ef87fd23342368b270149868d3ba
SpadyDong <47657625+SpadyDong@users.noreply.github.com>
2023-11-10 18:44:15 +0800

    Update point_cloud.h

--- cpp/point_cloud.h
+++ cpp/point_cloud.h
@@ -2,19 +2,23 @@
 #define _POINT_CLOUD_H
 
 #include <opencv2/opencv.hpp>
-#include <pcl/point_cloud.h>
 
 template<typename T>
-cv::Mat point2BEV(T pc, int width, int scale)
+cv::Mat cloudTopView(T pc, int width, float scale, bool show=false)
 {
     cv::Mat board = cv::Mat::zeros(width, width, CV_32FC1);
     int n = pc->size();
-    #pragma omp parallel for num_threads(4)
+#pragma omp parallel for num_threads(4)
     for(int i=0; i<n; i++)
     {
         int x = std::min(std::max(0, int(pc->points[i].x*scale+width/2)), width-1);
         int y = std::min(std::max(0, int(pc->points[i].y*scale+width/2)), width-1);
         board.at<float>(x,y) = 1;
+    }
+    if(show)
+    {
+        cv::imshow("BEV", board);
+        cv::waitKey(0);
     }
     return board;
 }
