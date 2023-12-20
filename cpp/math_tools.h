748703ab0bd12245e02ad9ffff2b6aa1c5a4919f
SpadyDong <47657625+SpadyDong@users.noreply.github.com>
2023-12-20 11:46:52 +0800

    Update math_tools.h

--- cpp/math_tools.h
+++ cpp/math_tools.h
@@ -1,9 +1,14 @@
 #ifndef _MATH_TOOLS_H
 #define _MATH_TOOLS_H
 
+/*
+一些常用的数学操作
+*/
+
 #include <Eigen/Dense>
 #include <pcl/point_cloud.h>
 
+// 计算pcl点云中点的深度
 template<typename T>
 float pointDepth(T p)
 {
@@ -11,8 +16,7 @@ float pointDepth(T p)
     return depth;
 }
 
-// 输入：一个四元数（实部和虚部）
-// 输出：一个3x3的旋转矩阵
+// 一个四元数（实部和虚部） => 一个3x3的旋转矩阵
 Eigen::Matrix3d quaternionToRotationMatrix(double w, double x, double y, double z)
 {
     Eigen::Matrix3d rotationMatrix;
@@ -38,6 +42,32 @@ Eigen::Matrix3d quaternionToRotationMatrix(double w, double x, double y, double 
     rotationMatrix(2, 2) = -sqx - sqy + sqz + sqw;
 
     return rotationMatrix;
+}
+
+// XYZ + 四元数 => 位姿矩阵
+Eigen::Matrix4d poseToMatrix(double x, double y, double z, double w, double qx, double qy, double qz)
+{
+    Eigen::Matrix4d poseMatrix = Eigen::Matrix4d::Identity();
+
+    Eigen::Matrix3d rotationMatrix = quaternionToRotationMatrix(w, qx, qy, qz);
+
+    poseMatrix.block<3, 3>(0, 0) = rotationMatrix;
+    poseMatrix(0, 3) = x;
+    poseMatrix(1, 3) = y;
+    poseMatrix(2, 3) = z;
+
+    return poseMatrix;
+}
+
+// XYZ + 四元数 => 位姿矩阵
+Eigen::Matrix4d poseToMatrix2(double x, double y, double z, double w, double qx, double qy, double qz)
+{
+    Eigen::Quaterniond qvl(w, qx, qy, qz);
+    Eigen::Vector3d tvl(x, y, z);
+    Eigen::Isometry3d Tvr(qvl);
+    Tvr.pretranslate(tvl);
+
+    return Tvr.matrix();
 }
 
 
